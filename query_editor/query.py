77e61a726fccd169ec9ebdf8ad62e0c9fb5bb6bd
Rusab Khan <74367699+RusabKhan@users.noreply.github.com>
2023-05-25 21:47:46 +0500

    graphs added

--- query_editor/query.py
+++ query_editor/query.py
@@ -10,14 +10,23 @@ configs = read_all_configs()
 global options
 options = []
 
-col1,col2 = st.columns([5,2])
-query_editor = st.container()
+
+
 connections = ""
 
 global metadata
+global df
 
 metadata = {"schema":[],"tables":[]}
 query = ""
+
+
+if 'query_df' not in st.session_state:
+    st.session_state['query_df'] = pd.DataFrame() 
+
+
+query_tab, graph_tab = st.tabs(["Query", "Graph"])
+
 
 def fetch_metadata():
     try:
@@ -33,34 +42,48 @@ def execute(query):
     try:
         metadata = read_config(connections)['data']
         del metadata['connection_type']
-        df =  Engine(**metadata).execute_query(query)
-        return df
+        return Engine(**metadata).execute_query(query)
     except Exception as e:
         return pd.DataFrame()
     
-    
-with query_editor:
-    with col1:
-        py_or_java = st.radio("Choose type",["Python","Java"])
-        if py_or_java =="Python":
-            options.extend(config["connection_name"] for config in configs['python'])
-        else:
-            options.extend(config["connection_name"] for config in configs['java'])
 
-        connections = st.selectbox("Select connection to use",options=options)
-        query = st.text_area(label="Query editor",placeholder="Select * from mytable",height=300)
-        metadata = fetch_metadata()
-    if submit := st.button("Submit"):
-        df = execute(query=query)
-        st.dataframe(df)
+with query_tab:
+    col1,col2 = query_tab.columns([5,2])
 
-    with col2:
-        st.write("Table Metadata")
-        for data in metadata["schema"]:
-            with st.expander(data):
-                lst = metadata["tables"][metadata["schema"].index(data)]
-                for i in lst:
-                    st.markdown(i)
+    with query_tab.container():
+        with col1:
+            py_or_java = st.radio("Choose type",["Python","Java"])
+            if py_or_java =="Python":
+                options.extend(config["connection_name"] for config in configs['python'])
+            else:
+                options.extend(config["connection_name"] for config in configs['java'])
+
+            connections = st.selectbox("Select connection to use",options=options)
+            query = st.text_area(label="Query editor",placeholder="Select * from mytable",height=300)
+            metadata = fetch_metadata()
+        if submit := st.button("Submit"):
+
+            df = execute(query=query)
+            st.session_state['query_df'] = df
+            st.dataframe(df)
+
+        with col2:
+            st.write("Table Metadata")
+            for data in metadata["schema"]:
+                with st.expander(data):
+                    lst = metadata["tables"][metadata["schema"].index(data)]
+                    for i in lst:
+                        st.markdown(i)
+        
+        
+with graph_tab:
+    choice = st.selectbox("Chart type",["Bar","Line","Area"])
+    df = st.session_state['query_df']
     
-    
+    if choice == "Bar":
+        st.bar_chart(df)
+    elif choice == "Line":
+        st.line_chart(df)
+    elif choice == "Area":
+        st.area_chart(df)
 
