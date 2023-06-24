73fe4210aa42ac1d959dce985b7f080e56b9d107
SyedMuhammadMuhsinKarim <syedmohsinkarim@gmail.com>
2023-06-24 23:36:13 +0500

    Added Schema Create Table

--- utils/schema_utils.py
+++ utils/schema_utils.py
@@ -1,80 +1,56 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, ARRAY
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATA_TYPE_MAPPING = {
+    str: String(255),
+    bool: Boolean,
+    int: Integer,
+    float: Float,
+    list: String(255)
+}
+
 class SchemaUtils:
-    """
-    A utility class for generating SQL statements for creating, altering, and deleting tables and schemas.
-    """
-    def __init__(self) -> None:
+    def __init__(self, connection_string):
+        self.engine = create_engine(connection_string)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+        self.Base = declarative_base()
+        
+    def create_dynamic_table(self, table_name):
+        
+        class DynamicTable(self.Base):
+            __tablename__ = table_name
+            id = Column(Integer, primary_key=True)
+
+        return DynamicTable
+
+    def create_table(self, table_name, json_data):
+        try:
+            DynamicTable = self.create_dynamic_table(table_name)
+
+            for key, value in json_data.items():
+                data_type = DATA_TYPE_MAPPING.get(type(value), String(255))
+                setattr(DynamicTable, key, Column(data_type))
+                    
+            self.Base.metadata.create_all(self.engine)
+            
+            print(f"Table '{table_name}' created.")
+            return True
+        except Exception as e:
+            return False
+
+    def alter_table(self, table_name, alteration):
         pass
-    
-    @staticmethod
-    def create_table(table_name: str, columns: list[str]) -> str:
-        """
-        Generate SQL statement for creating a table.
 
-        Args:
-            table_name (str): Name of the table.
-            columns (list[str]): List of column definitions as strings.
+    def drop_table(self, table_name):
+        pass
 
-        Returns:
-            str: SQL statement for creating the table.
-        """
-        column_defs = ', '.join(columns)
-        query = f"CREATE TABLE {table_name} ({column_defs})"
-        return query
+    def commit_changes(self):
+        self.session.commit()
 
-    @staticmethod
-    def alter_table(table_name: str, column_name: str, new_data_type: str) -> str:
-        """
-        Generate SQL statement for altering a table's column data type.
-
-        Args:
-            table_name (str): Name of the table.
-            column_name (str): Name of the column to be altered.
-            new_data_type (str): New data type for the column.
-
-        Returns:
-            str: SQL statement for altering the table's column data type.
-        """
-        query = f"ALTER TABLE {table_name} ALTER COLUMN {column_name} TYPE {new_data_type}"
-        return query
-
-    @staticmethod
-    def drop_table(table_name: str) -> str:
-        """
-        Generate SQL statement for dropping a table.
-
-        Args:
-            table_name (str): Name of the table.
-
-        Returns:
-            str: SQL statement for dropping the table.
-        """
-        query = f"DROP TABLE IF EXISTS {table_name}"
-        return query
-
-    @staticmethod
-    def create_schema(schema_name: str) -> str:
-        """
-        Generate SQL statement for creating a schema.
-
-        Args:
-            schema_name (str): Name of the schema.
-
-        Returns:
-            str: SQL statement for creating the schema.
-        """
-        query = f"CREATE SCHEMA {schema_name}"
-        return query
-
-    @staticmethod
-    def drop_schema(schema_name: str) -> str:
-        """
-        Generate SQL statement for dropping a schema.
-
-        Args:
-            schema_name (str): Name of the schema.
-
-        Returns:
-            str: SQL statement for dropping the schema.
-        """
-        query = f"DROP SCHEMA IF EXISTS {schema_name} CASCADE"
-        return query
+    def close_session(self):
+        self.session.close()
+        
+    def __exit__(self):
+        self.session.close()
