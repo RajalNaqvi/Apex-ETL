0e02f581111d3b835e1b6a4bb44976f3f98b0efd
SyedMuhammadMuhsinKarim <syedmohsinkarim@gmail.com>
2023-06-25 21:17:48 +0500

    Added Context Manager

--- utils/schema_utils.py
+++ utils/schema_utils.py
@@ -12,21 +12,55 @@ DATA_TYPE_MAPPING = {
 }
 
 class SchemaUtils:
-    def __init__(self, connection_string):
-        self.engine = create_engine(connection_string)
-        self.Session = sessionmaker(bind=self.engine)
-        self.session = self.Session()
+    is_session_close = True
+    def __init__(self, connection_string: str):
+        """
+        Initialize the SchemaUtils object.
+
+        Args:
+            connection_string (str): The connection string for the database.
+        """
+        self._engine = create_engine(connection_string)    
+        
+    def _create_session(self):
+        """
+        Create a new session and initialize metadata and base.
+        """
+        Session = sessionmaker(bind=self._engine)
+        self.session = Session()
         self.Base = declarative_base()
         self.metadata = MetaData()
         
-    def create_dynamic_table(self, table_name):
+        self.is_session_close = False
+        return True
+        
+    def create_dynamic_table(self, table_name: str):
+        """
+        Create a dynamic table class with the provided table name.
+
+        Args:
+            table_name (str): The name of the table.
+
+        Returns:
+            class: The dynamically created table class.
+        """
         class DynamicTable(self.Base):
             __tablename__ = table_name
             id = Column(Integer, primary_key=True)
 
         return DynamicTable
 
-    def create_table(self, table_name, json_data):
+    def create_table(self, table_name: str, json_data: dict):
+        """
+        Create a new table based on the provided table name and JSON data.
+
+        Args:
+            table_name (str): The name of the table.
+            json_data (dict): The JSON data containing column names and values.
+
+        Returns:
+            tuple: A tuple indicating the success status and a message.
+        """
         try:
             DynamicTable = self.create_dynamic_table(table_name)
 
@@ -34,7 +68,7 @@ class SchemaUtils:
                 data_type = DATA_TYPE_MAPPING.get(type(value), String(255))
                 setattr(DynamicTable, key, Column(data_type))
                     
-            self.Base.metadata.create_all(self.engine)
+            self.Base.metadata.create_all(self._engine)
             
             print(f"Table '{table_name}' created.")
             return True, f"Table '{table_name}' created."
@@ -48,33 +82,88 @@ class SchemaUtils:
         Args:
             table_name (str): The name of the table.
             column_name (str): The name of the column to be altered.
-            data_type (object): The desired `python datatype` methods for the column, enum: (`str`, `float`, `bool`, `float`, `list`).
+            data_type (object): The desired Python data type for the column. Valid values are `str`, `float`, `bool`, `int`, `list`.
 
-        Returns: 
+        Returns:
+            tuple: A tuple indicating the success status and a message.
         """
         try:
-            self.metadata.reflect(bind=self.engine, only=[table_name])
+            self.metadata.reflect(bind=self._engine, only=[table_name])
             existing_table = self.metadata.tables[table_name]
             
             DynamicTable = self.create_dynamic_table(existing_table)
             
-            mapped_data_type = self.DATA_TYPE_MAPPING.get(data_type, String(255))
+            mapped_data_type = DATA_TYPE_MAPPING.get(data_type, String(255))
             
             setattr(DynamicTable, column_name, Column(column_name, mapped_data_type))
-            DynamicTable.__table__.create(bind=self.engine, checkfirst=True)
+            DynamicTable.__table__.create(bind=self._engine, checkfirst=True)
             
             return True, f"`{table_name}` column `{column_name}` datatype Altered"
         except Exception as e:
             return False, str(e)
         
-    def drop_table(self, table_name):
+    def drop_table(self, table_name: str):
+        """
+        Drop the specified table from the database.
+
+        Args:
+            table_name (str): The name of the table to drop.
+        """
         pass
 
     def commit_changes(self):
+        """
+        Commit the changes made within the session.
+        """
         self.session.commit()
 
     def close_session(self):
+        """
+        Close the session and set the session close flag.
+        """
         self.session.close()
+        self.is_session_close = True
         
-    def __exit__(self):
-        self.session.close()
+    # Auto create and destroy session - if `with` context used
+    def __enter__(self):
+        """
+        Enter the `with` context and create a new session.
+        """
+        self._create_session()
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Exit the `with` context and commit changes, close the session.
+        """
+        self.commit_changes()
+        self.close_session()
+     
+# if __name__ == '__main__':
+#     connection_creds = {
+#         "username": "appx",
+#         "password": "appx",
+#         "host": "localhost",
+#         "port": "3306",
+#         "database": "mysql"
+#     }
+#     database_name = 'MySQL'
+#     from local.cache import database_engines
+#     database_connection = database_engines[database_name]
+#     connection_string = f"{database_connection}://{connection_creds['username']}:{connection_creds['password']}@{connection_creds['host']}:{connection_creds['port']}/{connection_creds['database']}"
+#     print(connection_string)
+#     json_data = {
+#         "name": "John Doe",
+#         "age": 25,
+#         "email": "johndoe@example.com",
+#         "is_active": True,
+#         "languages": ["Python", "JavaScript", "Java"]
+#     }
+
+#     utils = SchemaUtils(connection_string)
+
+#     # Create a new table
+#     table_name = 'user_table_new'
+#     # utils.create_table(table_name, json_data)
+#     print(utils.alter_table(table_name, "email", str))
+#     utils.close_session()
